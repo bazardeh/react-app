fe0160691830e9ba646faab54aa2547413c035bd
bazardeh <42860792+bazardeh@users.noreply.github.com>
2023-01-06 17:34:10 +0000

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Counter } from "./Counter";
-import ProductList from "./components/product-container";
 
 function App() {
-  console.log("🚀 ~ file: index.js:11 ~ App", App);
   return (
-    <>
-      <ProductList />
-      <hr />
-      <Counter />
-
-      <Counter value={10} />
-    </>
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React @@@
-    //     </a>
-    //   </header>
-    // </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React @@@
+        </a>
+      </header>
+    </div>
   );
 }
 
